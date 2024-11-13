efb216d9bb0c76d115cd1adbe41ff9142f38aa9a
fenil.gala <fenil.gala@vahvah.com>
2024-11-12 20:22:19 -0500

    Initial commit

---
+++ assignment1/srcs/server.c
@@ -0,0 +1,66 @@
+/*****************************************************************************
+ *
+ *     This file is part of Purdue CS 536.
+ *
+ *     Purdue CS 536 is free software: you can redistribute it and/or modify
+ *     it under the terms of the GNU General Public License as published by
+ *     the Free Software Foundation, either version 3 of the License, or
+ *     (at your option) any later version.
+ *
+ *     Purdue CS 536 is distributed in the hope that it will be useful,
+ *     but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *     GNU General Public License for more details.
+ *
+ *     You should have received a copy of the GNU General Public License
+ *     along with Purdue CS 536. If not, see <https://www.gnu.org/licenses/>.
+ *
+ *****************************************************************************/
+
+/*
+ * server.c
+ * Name:
+ * PUID:
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <errno.h>
+#include <string.h>
+#include <sys/socket.h>
+#include <sys/types.h>
+#include <netdb.h>
+#include <netinet/in.h>
+#include <errno.h>
+
+#define QUEUE_LENGTH 10
+#define RECV_BUFFER_SIZE 2048
+
+/* TODO: server()
+ * Open socket and wait for client to connect
+ * Print received message to stdout
+ * Return 0 on success, non-zero on failure
+ */
+int server(char *server_port)
+{
+  return 0;
+}
+
+/*
+ * main():
+ * Parse command-line arguments and call server function
+ */
+int main(int argc, char **argv)
+{
+  char *server_port;
+
+  if (argc != 2)
+  {
+    fprintf(stderr, "Usage: ./server-c (server port)\n");
+    exit(EXIT_FAILURE);
+  }
+
+  server_port = argv[1];
+  return server(server_port);
+}
